932cf4c330b32b6b8f9ae83f669d61c236d6dbab
hassantarif <hassantarif4@gmail.com>
2022-06-30 00:10:47 +0100

    add switch header functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,19 @@
 import {
   Route,
-  Routes, 
+  Routes,
+  useLocation
 } from "react-router-dom";
 import Header from "./components/header/header";
 import Home from "./components/home/Home";
 import Login from "./components/login/Login";
+import HeaderLogin from "./components/login/HeaderLogin";
+import { switchHeaders } from "./utils/switchHeader";
 
 const App = () => {
+  let location = useLocation();
   return (
     <div className="App">
-      <Header />
+      {switchHeaders("/login", HeaderLogin, location, Header)}
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Home />} />
--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -1,39 +1,9 @@
 import React from 'react'
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import * as Yup from "yup";
 
 const Login = () => {
   return (
     <div>
-      <Formik initialValues={{email: "", password: ""}}
-          validationSchema={Yup.object({
-            email: Yup.string()
-              .max(35, "must be 35 characters long")
-              .matches(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, "Invalid Email")
-              .trim("Cannot use Whitespaceing")
-              .required("Required"),
-            password: Yup.string()
-              .min(7, "must be at least 7")
-              .max(20, "Cannot be to long")
-              .required("Required")
-          })}
-          onSubmit={(values, { setSubmitting }) => {
-            console.log(values)
-            setSubmitting(false);
-          }}
-        >
-          <Form>
-            <label htmlFor="email">Email</label>
-            <Field name="email" type="email" className="bg-purple-400" placeholder="make correct email"/>
-            <ErrorMessage name="email" component="div" className="text-red-500 text-xs italic"/>
-
-            <label htmlFor="password">Password</label>
-            <Field name="password" type="password"/>
-            <ErrorMessage name="password" component="div" className="text-red-500 text-xs italic"/>
-
-            <input type="submit" value="Submit News"/>
-          </Form>
-      </Formik>
+      Login
     </div>
   )
 }
