c4e809f5078326d20bc449923aeebeeaeb93790f
hassantarif <hassantarif4@gmail.com>
2022-06-30 20:20:47 +0100

    all the components about the auth is setup neither sign_up I am working on it

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -9,7 +9,10 @@ const Header = ({user, logout}) => {
       <div className="flex justify-between">
         <LeftNav className="w-15 h-9 pr-5 relative left-8"/>
         <Search />
-        <RightNav user={user}/>
+        {user 
+          ? <RightNav user={user}/> : <>
+            
+          </>}
       </div>
     </div>
   )
