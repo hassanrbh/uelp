c120618ef9cffc19c54a2f21b21a8da3f4200eef
hassantarif <hassantarif4@gmail.com>
2022-07-12 17:02:13 +0100

    take some time to rest

--- frontend/src/components/businesses/subHeader.jsx
+++ frontend/src/components/businesses/subHeader.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import BusinessInfo from "./BusinessInfo";
 import Images from "./images";
 
 const SubHeader = () => {
   return (
     <>
       <Images />
+      <BusinessInfo />
     </>
   )
 }
