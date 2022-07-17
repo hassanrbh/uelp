9ff21384edc503028412ca9549bd0c9a5d76bb3e
hassantarif <hassantarif4@gmail.com>
2022-07-17 13:17:20 +0100

    adding share button , #need a modal

--- frontend/src/components/businesses/CustomButtons/Share.jsx
+++ frontend/src/components/businesses/CustomButtons/Share.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { ShareIcon } from "@heroicons/react/outline"
+import { useMutation, useQuery } from 'react-query';
+
+// todo: Adding Opening Modal LOGIC 
+// todo: Adding closing modal LOGIC
+// todo: set up in the api the api point that will access to save the biz
 
 const Share = () => {
+
+  const [openModal, setOpenModal] = useState(false);
+
+  const openingModal = (e) => {
+    e.preventDefault();
+    console.log("HELLO WORLD!");
+  }
+
   return (
-    <div>Share</div>
+    <button onClick={(e) => openingModal(e)} className="flex border border-[#c8c9ca] px-[16px] py-[7px] rounded text-black">
+      <ShareIcon className="h-6 w-6 mr-[6px]"/>
+      <p className="font-medium">Share</p>
+    </button>
   )
 }
 
