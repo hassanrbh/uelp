956793acb2f88222449c0a0e2d9963b02770fdeb
hassantarif <hassantarif4@gmail.com>
2022-07-18 14:03:09 +0100

    add a tracker down the delivery and takeout button

--- frontend/src/components/businesses/OrderFood/OrderFood.jsx
+++ frontend/src/components/businesses/OrderFood/OrderFood.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 const OrderFood = () => {
   const [toggleForDelivery, setIsToggleForDelivery] = useState(true);
@@ -8,25 +8,55 @@ const OrderFood = () => {
 
   const toggling = (e) => {
     if (e.target.dataset.id === "delivery") {
-      setIsActiveTakeout(prev => !prev);
-      setIsToggleForTakeout(prev => !prev);
-      setIsToggleForDelivery(prev => !prev);
-      setIsActiveDelivery(prev => !prev);
-      return ;
+      setIsActiveTakeout((prev) => !prev);
+      setIsToggleForTakeout((prev) => !prev);
+      setIsToggleForDelivery((prev) => !prev);
+      setIsActiveDelivery((prev) => !prev);
+      return;
     }
-    setIsActiveDelivery(prev => !prev)
-    setIsToggleForDelivery(prev => !prev);
-    setIsToggleForTakeout(prev => !prev);
-    setIsActiveTakeout(prev => !prev);
-  }
+    setIsActiveDelivery((prev) => !prev);
+    setIsToggleForDelivery((prev) => !prev);
+    setIsToggleForTakeout((prev) => !prev);
+    setIsActiveTakeout((prev) => !prev);
+  };
 
   return (
     <div className="border border-[#ebebeb] w-[363px] h-[303px] rounded ml-[30px]">
       <div className="m-4">
-        <h1 className="font-extrabold text-[rgb(45, 46, 47)] text-xl">Order Food</h1>
+        <h1 className="font-extrabold text-[rgb(45, 46, 47)] text-xl">
+          Order Food
+        </h1>
         <ul onClick={(e) => toggling(e)} className="flex ml-4 mt-5 gap-4">
-          <li data-id="delivery" className={`cursor-pointer  text-[#2d2e2f] font-[600] ${isActiveDelivery ? "text-[#2d2e2f]" : "text-[#6E7083]"} text-[16px]`}>Delivery</li>
-          <li data-id="takeout" className={`cursor-pointer text-[#2d2e2f] text-[16px]  ${isActiveTakeout ? "text-[#2d2e2f]" : "text-[#6E7083]"} font-[600]`}>Takeout</li>
+          <div>
+            <li
+              data-id="delivery"
+              className={`transition-all ease-in-out cursor-pointer  text-[#2d2e2f] font-[600] ${
+                isActiveDelivery ? "text-[#2d2e2f]" : "text-[#6E7083]"
+              } text-[17px]`}
+            >
+              Delivery
+            </li>
+            {toggleForDelivery ? (
+              <>
+                <div className="bg-[#e00707] bottom-0 h-[4px] left-0 rounded mt-[2px] opacity-100 transition-all"></div>
+              </>
+            ) : null}
+          </div>
+          <div>
+            <li
+              data-id="takeout"
+              className={`transition-all ease-in-out cursor-pointer text-[#2d2e2f] text-[17px]  ${
+                isActiveTakeout ? "text-[#2d2e2f]" : "text-[#6E7083]"
+              } font-[600]`}
+            >
+              Takeout
+            </li>
+            {toggleForTakeout ? (
+              <>
+                <div className="bg-[#e00707] bottom-0 h-[4px] left-0 rounded mt-[2px] transition-all"></div>
+              </>
+            ) : null}
+          </div>
         </ul>
         {toggleForDelivery ? <div>Hello</div> : null}
         {toggleForTakeout ? <div>World</div> : null}
