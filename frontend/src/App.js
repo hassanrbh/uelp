f2312b800cf5ccb18bc2d4af0a89bf5324b8b4f6
hassantarif <hassantarif4@gmail.com>
2022-07-19 20:38:04 +0100

    :art: improve the view of routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,37 +3,51 @@ import { useState, lazy } from "react";
 import { switchHeaders } from "./utils/switchHeader";
 import { useQuery } from "react-query";
 
+// Services 
 import UserService from "./services/auth.service";
 import AuthService from "./services/user.service";
+
+// Utils
 import SuspenseLazy from "./utils/suspense_lazy";
 import NonExistError from "./components/profile/NonExistError";
 import Header from "./components/header/header";
 
-const UnitBusiness = lazy(() => import("./components/businesses/UnitBusiness"));
-const Contact = lazy(() => import("./components/contacts/contact"));
-const Home = lazy(() => import("./components/home/Home"));
+// Auth Components
+// const Register = lazy(() => import("./components/register/Register"));
 const Login = lazy(() => import("./components/login/Login"));
+const Logout = lazy(() => import("./components/profile/Logout"));
 const HeaderLogin = lazy(() => import("./components/login/HeaderLogin"));
-const Error = lazy(() => import("./components/errors/Error"));
-// const  Register = lazy(() => import("./components/register/Register"));
 const ProtectedRoute = lazy(() => import("./utils/protectedRoute"));
-const Yelper = lazy(() => import("./components/profile/Yelper"));
+
+// Profile Components
+const Contact = lazy(() => import("./components/contacts/contact"));
 const Profile = lazy(() => import("./components/profile/Profile"));
 const Messages = lazy(() => import("./components/profile/Messages"));
 const Notifications = lazy(() => import("./components/profile/Notifications"));
 const Settings = lazy(() => import("./components/profile/Settings"));
 const Friends = lazy(() => import("./components/profile/Friends"));
-const Logout = lazy(() => import("./components/profile/Logout"));
-const WriteReview = lazy(() => import("./components/profile/WriteReview"));
-const BusinessHome = lazy(() => import("./components/businesses/BusinessHome"));
-const Search = lazy(() => import("./components/search/search"));
 const UserPhotos = lazy(() => import("./components/profile/UserPhotos"));
 const AddUserPhotos = lazy(() => import("./components/profile/AddUserPhotos"));
 const FindFriends = lazy(() => import("./components/profile/FindFriends"));
+
+// Home Page
+const Home = lazy(() => import("./components/home/Home"));
+
+// Error Components 
+const Error = lazy(() => import("./components/errors/Error"));
+
+// Businesses Components
+const UnitBusiness = lazy(() => import("./components/businesses/UnitBusiness"));
+const Yelper = lazy(() => import("./components/profile/Yelper"));
+const WriteReview = lazy(() => import("./components/profile/WriteReview"));
+const BusinessHome = lazy(() => import("./components/businesses/BusinessHome"));
+const Search = lazy(() => import("./components/search/search"));
 const BizUserPhotos = lazy(() =>
   import("./components/businesses/BusinessesPhotos/BizUserPhotos")
 );
 const Order = lazy(() => import("./components/order/Order"));
+
+// Menu Components
 const Menu = lazy(() => import("./components/Menu/Menu"));
 
 const App = () => {
@@ -64,125 +78,44 @@ const App = () => {
         Header,
         currentUser,
         logout
+
       )}
+
       <Routes>
+
+        {/* Auth Components */}
         <Route path="/login" element={<SuspenseLazy element={<Login />} />} />
-        <Route
-          path="/user_photos"
-          element={
-            <ProtectedRoute>
-              <SuspenseLazy element={<UserPhotos />}></SuspenseLazy>
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/menu/:menu_name"
-          element={
-            <ProtectedRoute>
-              <SuspenseLazy element={<Menu />} />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/user_photos/add"
-          element={
-            <ProtectedRoute>
-              <SuspenseLazy element={<AddUserPhotos />} />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/order"
-          element={
-            <ProtectedRoute>
-              <SuspenseLazy element={<Order />} />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/biz_user_photos"
-          element={
-            <ProtectedRoute>
-              <SuspenseLazy element={<BizUserPhotos />} />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/profile/messages"
-          element={
-            <ProtectedRoute>
-              <SuspenseLazy element={<Messages />} />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/profile/find-friends"
-          element={
-            <ProtectedRoute>
-              <SuspenseLazy element={<FindFriends />} />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/profile/notifications"
-          element={
-            <ProtectedRoute>
-              <SuspenseLazy element={<Notifications />} />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/profile"
-          element={
-            <ProtectedRoute>
-              <SuspenseLazy element={<Profile />} />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/profile/account_settings"
-          element={
-            <ProtectedRoute>
-              <SuspenseLazy element={<Settings />} />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/profile/logout"
-          element={<SuspenseLazy element={<Logout />} />}
-        />
-        <Route
-          path="/profile/friends"
-          element={
-            <ProtectedRoute>
-              <SuspenseLazy element={<Friends />} />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="/search" element={<SuspenseLazy element={<Search />} />} />
-        <Route
-          path="/biz"
-          element={<SuspenseLazy element={<BusinessHome />} />}
-        />
-        <Route
-          path="/biz/:business_name"
-          element={<SuspenseLazy element={<UnitBusiness />} />}
-        />
-        <Route
-          path="/writereview"
-          element={<SuspenseLazy element={<WriteReview />} />}
-        />
-        <Route
-          path="/contacts/:contact_id"
-          element={<SuspenseLazy element={<Contact />} />}
-        />
+        
+        {/* Profile Components */}
+        <Route path="/user_photos" element={<ProtectedRoute><SuspenseLazy element={<UserPhotos />}></SuspenseLazy></ProtectedRoute>}/>
+        <Route path="/user_photos/add" element={<ProtectedRoute><SuspenseLazy element={<AddUserPhotos />} /></ProtectedRoute>}/>
+        <Route path="/profile/messages" element={<ProtectedRoute><SuspenseLazy element={<Messages />} /></ProtectedRoute>}/>
+        <Route path="/profile/find-friends" element={<ProtectedRoute><SuspenseLazy element={<FindFriends />} /></ProtectedRoute>}/>
+        <Route path="/profile/notifications" element={<ProtectedRoute><SuspenseLazy element={<Notifications />} /></ProtectedRoute>}/>
+        <Route path="/profile" element={<ProtectedRoute><SuspenseLazy element={<Profile />} /></ProtectedRoute>}/>
+        <Route path="/profile/account_settings" element={<ProtectedRoute><SuspenseLazy element={<Settings />} /></ProtectedRoute>}/>
+        <Route path="/profile/logout" element={<SuspenseLazy element={<Logout />} />}/>
+        <Route path="/profile/friends" element={<ProtectedRoute><SuspenseLazy element={<Friends />} /></ProtectedRoute>}/>
+        <Route path="/contacts/:contact_id" element={<SuspenseLazy element={<Contact />} />}/>
         <Route path="/profile/nonexisted" element={<NonExistError />} />
-        <Route
-          path="/user_details"
-          element={<SuspenseLazy element={<Yelper />} />}
-        />
-        <Route path="/" element={<SuspenseLazy element={<Home />} />} />
+        <Route path="/user_details" element={<SuspenseLazy element={<Yelper />} />}/>
+
+        {/* Business Components */}
+        <Route path="/order" element={<ProtectedRoute><SuspenseLazy element={<Order />} /></ProtectedRoute>}/>
+        <Route path="/biz_user_photos" element={<ProtectedRoute><SuspenseLazy element={<BizUserPhotos />} /></ProtectedRoute>}/>
+        <Route path="/search" element={<SuspenseLazy element={<Search />} />} />
+        <Route path="/biz" element={<SuspenseLazy element={<BusinessHome />} />}/>
+        <Route path="/biz/:business_name" element={<SuspenseLazy element={<UnitBusiness />} />}/>
+        <Route path="/writereview" element={<SuspenseLazy element={<WriteReview />} />}/>
+
+        {/* Error Components */}
         <Route path="*" element={<SuspenseLazy element={<Error />} />} />
+
+        {/* Menu Components */}
+        <Route path="/menu/:menu_name" element={<ProtectedRoute><SuspenseLazy element={<Menu />} /></ProtectedRoute>}/>
+
+        {/* Home Components */}
+        <Route path="/" element={<SuspenseLazy element={<Home />} />} />
       </Routes>
     </div>
   );
