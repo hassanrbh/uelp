63ab4924ac6cf339c6ee0fa70042111d8ff884d4
hassantarif <hassantarif4@gmail.com>
2022-07-21 16:01:32 +0100

    :bug: api endpoint for working hours fix

--- frontend/src/components/businesses/BusinesseWorkingHours/HoursOfWorking.jsx
+++ frontend/src/components/businesses/BusinesseWorkingHours/HoursOfWorking.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import { useQuery } from "react-query";
+import client from "../../../services/react-query";
+import WorkingHours from "../../../services/working_hours.service"
+import Loading from "../../reusableComponents/Loading"
 
 const HoursOfWorking = () => {
-  return (
-    <div>HoursOfWorking</div>
-  )
-}
+  const name = client.getQueryData(["unit-business"]).profile.private_details.name;
+  const {data, isLoading, isSuccess} = useQuery(["working_hours_for", name], () => WorkingHours.getWorkingHours(name))
 
-export default HoursOfWorking
+  if (isLoading) return <Loading />
+
+  return isSuccess ? console.log(data) : null
+};
+
+export default HoursOfWorking;
--- frontend/src/services/working_hours.service.js
+++ frontend/src/services/working_hours.service.js
@@ -3,13 +3,14 @@ import authHeader from "./auth_header";
 
 const API_URL = "http://localhost:3000/api/v1/businesses/";
 
-class WorkingHour {
+class WorkingHours {
   async getWorkingHours(business_slug) {
-    const { data } = axios.get(API_URL + `${business_slug}/working_hours`, {
+    const { data } = await axios.get(API_URL + `${business_slug}/working_hours`, {
       headers: authHeader(),
     });
+    console.log(data)
     return data;
   }
 }
 
-export default WorkingHour;
+export default new WorkingHours();
