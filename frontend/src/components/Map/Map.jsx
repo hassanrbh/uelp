7b0f7dc50de84b4ece7a7c29f33d8433847d5dff
hassantarif <hassantarif4@gmail.com>
2022-07-21 14:56:02 +0100

    :art: storing every business location latitude and longitude in the db for quick get the map

--- frontend/src/components/Map/Map.jsx
+++ frontend/src/components/Map/Map.jsx
@@ -1,14 +1,10 @@
 import React from "react";
-import { Link, useParams, useLocation } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 const Map = () => {
   const { business_address } = useParams();
-  const location = useLocation();
 
   return <div>
-    <Link to={`/${location.hash.slice(1)}/${business_address}`}>      
-      Back 
-    </Link>
     <h1>{business_address}</h1>
   </div>;
 };
--- frontend/src/components/businesses/Map/Map.jsx
+++ frontend/src/components/businesses/Map/Map.jsx
@@ -1,44 +1,46 @@
 import React, { useState } from "react";
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
-import { useQuery } from "react-query";
-import IpTracker from "../../../api/ip_info";
+// import { useQuery } from "react-query";
+// import IpTracker from "../../../api/ip_info";
 import client from "../../../services/react-query";
 import MapSkeleton from "./MapSkeleton";
 require("react-leaflet-markercluster/dist/styles.min.css");
 
 const Map = () => {
-  const [combLongLit, setCombLongLit] = useState([]);
-  const address = client.getQueryData(["unit-business"]).profile?.business_details?.address;
-  const { isLoading, isSuccess, error, isError } = useQuery(
-    "map-api-tracker-for-businesses",
-    () => IpTracker.getPositionStack(address),
-    {
-      onSuccess: (data) => {
-        if (data) {
-          const longitude = data?.data[0]?.longitude;
-          const latitude = data?.data[0]?.latitude;
-          setCombLongLit([latitude, longitude]);
-        } else {
-          const longitude = Math.random(200)
-          const latitude = Math.random(200)
-          setCombLongLit([latitude, longitude]);
-        }
-      },
-      refetchOnWindowFocus: false,
-      refetchOnmount: true,
-      refetchOnReconnect: true,
-      retry: false,
-      staleTime: Infinity,
-    }
-  );
+  const latitude = client.getQueryData(["unit-business"]).profile?.coords_details?.latitude;
+  const longitude = client.getQueryData(["unit-business"]).profile?.coords_details?.longitude;
+  // const [combLongLit, setCombLongLit] = useState([]);
+  // const address = client.getQueryData(["unit-business"]).profile?.business_details?.address;
+  // const { isLoading, isSuccess, error, isError } = useQuery(
+  //   "map-api-tracker-for-businesses"
+  //   () => IpTracker.getPositionStack(address),
+  //   {
+  //     onSuccess: (data) => {
+  //       if (data) {
+  //         const longitude = data?.data[0]?.longitude;
+  //         const latitude = data?.data[0]?.latitude;
+  //         setCombLongLit([latitude, longitude]);
+  //       } else {
+  //         const longitude = Math.random(200)
+  //         const latitude = Math.random(200)
+  //         setCombLongLit([latitude, longitude]);
+  //       }
+  //     },
+  //     refetchOnWindowFocus: false,
+  //     refetchOnmount: true,
+  //     refetchOnReconnect: true,
+  //     retry: false,
+  //     staleTime: Infinity,
+  //   }
+  // );
 
-  if (isLoading) return <MapSkeleton />;
-  if (isError) return <div>{error}</div>;
+  // if (isLoading) return <MapSkeleton />;
+  // if (isError) return <div>{error}</div>;
 
-  return (isSuccess && combLongLit.length === 2) ? (
-    <MapContainer
+  return (
+    (longitude && latitude) ? <MapContainer
       className="markercluster-map"
-      center={combLongLit}
+      center={[latitude, longitude]}
       zoom={13}
       maxZoom={18}
       
@@ -48,9 +50,9 @@ const Map = () => {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
 
-      <Marker position={combLongLit} />
-    </MapContainer>
-  ) : null;
+      <Marker position={[latitude, longitude]} />
+    </MapContainer> : <MapSkeleton />
+  )
 };
 
 export default Map;
