74f6df7b53849a4e12b4a6cd4d635bf93bc0ba2e
hassantarif <hassantarif4@gmail.com>
2022-07-22 19:56:53 +0100

    :necktie: structure the elements into a grid

--- frontend/src/components/businesses/Amenties/Amenties.jsx
+++ frontend/src/components/businesses/Amenties/Amenties.jsx
@@ -16,17 +16,19 @@ import {
   ScaleIcon,
   XIcon,
   CreditCardIcon,
+  TruckIcon,
 } from "@heroicons/react/outline";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faUtensils,
+  faBasketShopping,
   faLightbulb,
   fachild,
-  famoped,
   fatv,
   faburgerfries,
   fawheelchair,
 } from "@fortawesome/free-solid-svg-icons";
+import Divider from "../../reusableComponents/Dividor"
 
 const Amenties = () => {
   const business_slug = client.getQueryData(["unit-business"]).profile
@@ -40,28 +42,48 @@ const Amenties = () => {
   if (isLoading) return <Loading />;
   if (isError) return <div>{error}</div>;
 
+  const CapitalizeWords = (str) => {
+    const words = str.split(" ");
+    return words
+      .map((word) => {
+        return word[0].toUpperCase() + word.substring(1);
+      })
+      .join(" ");
+  };
+
   return isSuccess ? (
     <div>
       <h1 className="font-bold text-[1.25rem] mb-4">Amenties and More</h1>
-      {Object.keys(data.amenties)
-        .slice(0, 4)
-        .map((item, _) => (
-          <div>{item}</div>
-        ))}
-      {toggle ? (
-        <>
-          {Object.keys(data.amenties)
-            .slice(4, -1)
-            .map((item, _) => (
-              <div>{item}</div>
-            ))}
-        </>
-      ) : null}
+      <div className="grid grid-cols-2 gap-3">
+        {Object.keys(data.amenties)
+          .slice(0, 4)
+          .map((item, _) => (
+            <div className="text-[17px] font-medium flex">
+              {item === "offers_delivery" ? <TruckIcon className="h-6 w-6 mr-3"/> : null}
+              {item === "vegan_options" ? <FontAwesomeIcon icon={faUtensils} className="h-6 w-6 mr-3"/> : null}
+              {item === "offers_takeout" ? <FontAwesomeIcon icon={faBasketShopping} className="h-6 w-6 mr-3"/> : null}
+              {item === "accept_credit_cards" ? <CreditCardIcon className="h-6 w-6 mr-3"/> : null}
+              {CapitalizeWords(item.replace(/[|&;$_%@"<>()+,]/g, " "))}
+            </div>
+          ))}
+        {toggle ? (
+          <>
+            {Object.keys(data.amenties)
+              .slice(4, -1)
+              .map((item, _) => (
+                <div className="text-[16px] font-[600]">
+                  {CapitalizeWords(item.replace(/[|&;$_%@"<>()+,]/g, " "))}
+                </div>
+              ))}
+          </>
+        ) : null}
+      </div>
       <MoreAmenties
         count={Object.keys(data.amenties).slice(4, -1).length}
         setToggle={setToggle}
         toggle={toggle}
       />
+      <Divider />
     </div>
   ) : null;
 };
--- frontend/src/components/businesses/Amenties/MoreAmenties.jsx
+++ frontend/src/components/businesses/Amenties/MoreAmenties.jsx
@@ -4,7 +4,7 @@ const MoreAmenties = ({count, setToggle, toggle}) => {
   return (
     <button
       to="/biz_user_photos"
-      className="flex border border-[#c8c9ca] px-[16px] py-[7px] rounded text-black"
+      className="flex border border-[#c8c9ca] px-[16px] py-[7px] rounded text-black mt-5 mb-3"
       onClick={() => setToggle(prev => !prev)}
     >
       {!toggle ? <span className="mr-[4px] font-medium">{count}</span> : null}
