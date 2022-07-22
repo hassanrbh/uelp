b9c4c596e04e2f7a27afa12f21000f3f7fdd2b67
hassantarif <hassantarif4@gmail.com>
2022-07-22 17:50:58 +0100

    :pencil2: fix the modal and the z-index Map

--- frontend/src/components/reusableComponents/Modal.jsx
+++ frontend/src/components/reusableComponents/Modal.jsx
@@ -14,15 +14,14 @@ const Modal = ({ setSwitcher }) => {
       aria-modal="true"
     >
       <div
-        className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
-        ref={ref}
+        className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity "
       ></div>
 
       <div className="fixed z-10 inset-0 overflow-y-auto">
-        <div className="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0">
-          <div className="relative bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full">
+        <div className="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0 z-[1000]">
+          <div className="relative bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full" ref={ref}>
             <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-              <div className="sm:flex sm:items-start">
+              <div className="sm:flex sm:items-start" >
                 <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
                   <svg
                     className="h-6 w-6 text-red-600"
