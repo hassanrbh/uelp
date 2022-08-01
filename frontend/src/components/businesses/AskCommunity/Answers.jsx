0de8267fe5a426a0a7ee7f7efc6da37c2de577b1
hassantarif <hassantarif4@gmail.com>
2022-08-01 02:42:13 +0100

    :art: more and less buttons for text that are too mush

--- frontend/src/components/businesses/AskCommunity/Answers.jsx
+++ frontend/src/components/businesses/AskCommunity/Answers.jsx
@@ -1,16 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import Loadmore from "./loadmore";
 import { Link } from "react-router-dom";
 
 const Answers = ({ answer, question }) => {
+  const [more, isMore] = useState(false);
   return (
     <>
       <div className="font-[400] flex text-base mt-3">
         <span className="min-w-[30px] font-bold max-w-[797px]">A: </span>
-        <p className="max-w-[797px]">{answer?.answer}</p>
+        <p className="max-w-[797px]">
+          {answer?.answer.length >= 350 ? (
+            <div>
+              <p>
+                {answer.answer.slice(0, 350)}
+                {more ? answer.answer.slice(350, -1) : "..."}{" "}
+                {more ? <span
+                  onClick={() => isMore((prev) => !prev)}
+                  className="font-bold text-[rgba(2,122,151,1)] hover:underline cursor-pointer"
+                >
+                  less
+                </span> : <span
+                  onClick={() => isMore((prev) => !prev)}
+                  className="font-bold text-[rgba(2,122,151,1)] hover:underline cursor-pointer"
+                >
+                  more
+                </span>}
+              </p>
+            </div>
+          ) : (
+            <p>{answer?.answer}</p>
+          )}
+        </p>
       </div>
       <div className="font-normal mt-1 text-sm  text-[rgba(110,112,114,1)] gap-x-2 flex ml-[10px] relative left-[22px]">
-        <Link to={`/user_details?username=${answer?.answerer?.username}`} className="font-semibold text-[rgba(2,122,151,1)] hover:underline">
+        <Link
+          to={`/user_details?username=${answer?.answerer?.username}`}
+          className="font-semibold text-[rgba(2,122,151,1)] hover:underline"
+        >
           {answer?.answerer?.username?.charAt(0).toUpperCase()}
           {answer.answerer.username?.slice(1)}.
         </Link>
