ce68b3086c72132b5ea94d6ba7156e1f0b46a5dd
hassantarif <hassantarif4@gmail.com>
2022-08-07 12:07:23 +0100

    :zap: share controller from 600ms to (77ms to 300ms) to send a message

--- app/controllers/api/v1/share_controller.rb
+++ app/controllers/api/v1/share_controller.rb
@@ -4,20 +4,22 @@ module Api
   module V1
     class ShareController < ApplicationController
       before_action :authenticate_user!
+
       def create
         expected_user = check_if_receiver_exists_as_yelper
+
         if expected_user
-          business = Business.find_by_name(params[:business_slug])
+          @business = Business.find_by_name(params[:business_slug])
           @share = Share.new(share_params)
           @share.user = current_user
-          @share.business = business
+          @share.business = @business
+
           begin
             if @share.save
               if !@share.to.match(Devise.email_regexp)
-                to = User.find_by_username(@share.to)
                 UserMailer.share_note(
                   current_user,
-                  to.email,
+                  expected_user.email,
                   @share.note
                 ).deliver_now
               else
@@ -27,25 +29,29 @@ module Api
                   @share.note
                 ).deliver_now
               end
+
               render json: { send: "Sent" }, status: :ok
             else
-              render json: @share.errors.full_messages, status: :not_found
+              render json: @share.errors.full_messages, status: 403
             end
           rescue ActiveRecord::RecordNotUnique
-            render json: { error: ["Email already Sent"] }, status: :not_found
+            render json: { error: ["Email already Sent"] }, status: :found
           end
         else
           render json: {
-                  to: ["Yelper username not exist, use Email instead"]
+                  to: ["Yelper user dosen't exist, use Email instead"]
                 },
-              status: :not_found
+                status: :not_found
         end
       end
 
       def check_if_receiver_exists_as_yelper
+        # we check if share to propertie matches the email regex pattern
         unless params[:share][:to].match(Devise.email_regexp)
+          # we see if that username arlready in the database
           expected_user = User.find_by_username(params[:share][:to])
-          return true if expected_user.present? && !expected_user.nil?
+
+          return expected_user if expected_user.present? && !expected_user.nil?
 
           return false
         end
