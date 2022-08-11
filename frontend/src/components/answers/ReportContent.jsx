2e7a00e4500fbe9c9e493baf1461264863e0795d
hassantarif <hassantarif4@gmail.com>
2022-08-11 14:37:50 +0100

    adding the model

--- frontend/src/components/answers/ReportContent.jsx
+++ frontend/src/components/answers/ReportContent.jsx
@@ -1,7 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useMutation } from 'react-query';
+import Select from 'react-select';
+import { TextField } from '@mui/material';
+
+const reportOptions = [
+  {
+    value: "it dosen't answer the question asked",
+    label: "it dosen't answer the question asked"
+  },
+  {
+    value: 'it contains threats, lewdness or hate speech',
+    label: 'it contains threats, lewdness or hate speech'
+  },
+  {
+    value: 'it violates uelp standard privacy',
+    label: 'it violates uelp standard privacy'
+  },
+  {
+    value: 'it contains promotional material',
+    label: 'it contains promotional material'
+  }
+];
 
 const ReportContent = ({ answer }) => {
-  return <div>{answer?.answer}</div>;
+  const [reportValue, setReportValue] = useState('');
+  const submitReport = (e) => {};
+  const [moreDetails, setMoreDetails] = useState('');
+
+  return (
+    <div className="">
+      <Select
+        options={reportOptions}
+        onChange={(e) => setReportValue(e.value)}
+        placeholder="Why do you want to report this answer ?"
+        className="text-left rounded text-black m-[5px]"
+        menuPortalTarget={document.body}
+        styles={{ menuPortal: (base) => ({ ...base, zIndex: 9999 }) }}
+      />
+      {reportValue ? (
+        <form onSubmit={(e) => submitReport(e)}>
+          <TextField
+            label="Please provide specific details below:"
+            color="primary"
+            variant={'outlined'}
+            margin={'normal'}
+            sx={{ width: '100%' }}
+            value={moreDetails}
+            onChange={(e) => setMoreDetails(e.currentTarget.value)}
+            autoComplete="off"
+            multiline
+            minRows={3}
+            maxRows={10}
+            InputProps={{
+              className: 'p-0 h-[100px] z-0'
+            }}
+          />
+
+          <input type="submit" value="Send" />
+        </form>
+      ) : null}
+    </div>
+  );
 };
 
 export default ReportContent;
