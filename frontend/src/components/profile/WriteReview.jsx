ca9704c6923a725011eaa5a14b53e22d18261fd9
hassantarif <hassantarif4@gmail.com>
2022-08-12 18:57:27 +0100

    :lipstick: adding a textarea for description

--- frontend/src/components/profile/WriteReview.jsx
+++ frontend/src/components/profile/WriteReview.jsx
@@ -40,7 +40,7 @@ const WriteReview = () => {
                 {currentStatusStar}
               </p>
             </div>
-            <input type="textarea" className="w-full" placeholder="Doesn't look like much when you walk past, but I was practically dying of hunger so I popped in. The definition of a hole-in-the-wall. I got the regular hamburger and wow…  there are no words. A classic burger done right. Crisp bun, juicy patty, stuffed with all the essentials (ketchup, shredded lettuce, tomato, and pickles). There`s about a million options available between the menu board and wall full of specials, so it can get a little overwhelming, but you really can`t go wrong. Not much else to say besides go see for yourself! You won't be disappointed. " />
+            <textarea className="w-full h-[216px] mt-3 titiz" placeholder={"Doesn't look like much when you walk past, but I was practically dying of hunger so I popped in. The definition of a hole-in-the-wall. I got the regular hamburger and wow…  there are no words. A classic burger done right. Crisp bun, juicy patty, stuffed with all the essentials (ketchup, shredded lettuce, tomato, and pickles). There`s about a million options available between the menu board and wall full of specials, so it can get a little overwhelming, but you really can`t go wrong. Not much else to say besides go see for yourself! You won't be disappointed."}></textarea>
           </div>
         </main>
       </div>
